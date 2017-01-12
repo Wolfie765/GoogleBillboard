fc4207b2bcf6629f0afe00eb26430044029e2dea
Wolfie765 <adbarrales@s.sfusd.edu>
2017-01-12 11:35:26 -0800

    IODASPTJOtmfgv rh

--- GoogleBillboard.pde
+++ GoogleBillboard.pde
@@ -1,24 +1,35 @@
 public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
 public void setup()  
 	{   
-
-	String sEction = e.substring(2, 12);
-    double dNum = Double.parseDouble(sEction);
-    System.out.println(dNum);
-	
+	for(int i = 2; i < e.length()-1; i++)
+    	{
+    		String sEction = e.substring(i, i + 10);
+    		double dNum = Double.parseDouble(sEction);
+    		if(isPrime(dNum) == true)
+    			{
+    			 System.out.println(dNum);
+    			 break;
+    			}
         noLoop();  
+    	}
+    
 	}  
 public void draw()  
 {   
 	//not needed for this assignment
-}  
+}
+
 public boolean isPrime(double dNum)  
 {   
     //to be finished later
-    for(int d = 0; d < Math.sqrt(dNum); d++)
-    {	
+    if(dNum < 2)
+    	return false;
+    for(int d = 2; d < Math.sqrt(dNum); d++)
+    {	 	
     	if(dNum % d ==0)
     		return false;  
     }
     return true;
-} 
+}
+
+
--- build-tmp/source/GoogleBillboard.java
+++ build-tmp/source/GoogleBillboard.java
@@ -17,23 +17,38 @@ public class GoogleBillboard extends PApplet {
 public final static String e = "2.718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059";  
 public void setup()  
 	{   
-
-	String sEction = e.substring(2, 12);
-    double dNum = Double.parseDouble(sEction);
-    System.out.println(dNum);
-	
+	for(int i = 2; i < e.length()-1; i++)
+    	{
+    		String sEction = e.substring(i, i + 10);
+    		double dNum = Double.parseDouble(sEction);
+    		if(isPrime(dNum) == true)
+    			{
+    			 System.out.println(dNum);
+    			 break;
+    			}
         noLoop();  
+    	}
+    
 	}  
 public void draw()  
 {   
 	//not needed for this assignment
-}  
+}
+
 public boolean isPrime(double dNum)  
 {   
-    //to be finished later   
-    
-    return true;  
-} 
+    //to be finished later
+    if(dNum < 2)
+    	return false;
+    for(int d = 2; d < Math.sqrt(dNum); d++)
+    {	 	
+    	if(dNum % d ==0)
+    		return false;  
+    }
+    return true;
+}
+
+
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "GoogleBillboard" };
     if (passedArgs != null) {
